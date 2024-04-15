cd114ac8d3fb15b94335b56df8a8e8608e2e7b6a
Hamzshaa <trident32000@gmail.com>
2024-04-16 00:21:00 +0300

    trpc setup finished

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { nextApp, nextHandler } from "./next-utils";
 import { getPayloadClient } from "./get-payload";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
+
 const start = async () => {
-  console.log("Dkfja");
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
@@ -15,6 +24,14 @@ const start = async () => {
       },
     },
   });
+
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  );
 
   app.use((req, res) => nextHandler(req, res));
 
